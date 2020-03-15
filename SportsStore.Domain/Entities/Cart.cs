5bf2fa9c18ed76f9e388ea7ef8ae70a0b482681d
Alex-made <alekcey94@gmail.com>
2020-03-15 15:50:33 +0300

    Добавил функциональность корзины

---
+++ SportsStore.Domain/Entities/Cart.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities
+{
+    class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Product product, int quantity)
+        {
+            //добавить продукт и количество в картлайн
+            CartLine Line = lineCollection.Where(p => p.product.ProductID == product.ProductID).FirstOrDefault();
+            if (Line == null)
+            {
+                CartLine NewLine = new CartLine();
+                NewLine.product = product;
+                NewLine.Quantity = quantity;
+
+                lineCollection.Add(Line);
+            }
+            else
+            {
+                Line.Quantity = quantity;
+            }
+
+        }
+
+        public void RemoveLine(Product product)
+        {
+            lineCollection.RemoveAll(l => l.Product.ProductID == product.ProductID);
+        }
+
+        public int ComputeTotalValue()
+        {
+            return lineCollection.Sum(p => p.product.Price * p.Quantity);
+        }
+
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+    }
+}
---
+++ SportsStore.Domain/Entities/CartLine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities
+{
+    class CartLine
+    {
+        public Product product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
