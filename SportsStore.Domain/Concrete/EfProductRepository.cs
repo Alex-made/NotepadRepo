43a4d0c6b01cdccdbc32f6e8bff1b3ed18942351
Alex-made <alekcey94@gmail.com>
2020-03-15 19:58:56 +0300

    Добавил контекст и репозиторий

---
+++ SportsStore.Domain/Concrete/EfProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Linq;
+
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EfProductRepository : IProductRepository
+    {
+        private EfDbContext Context = new EfDbContext();
+
+        public IQueryable<Product> Products
+        {
+            get { return Context.Products; }
+        }
+
+    }
+}
